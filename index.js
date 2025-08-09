e9055e70dc7ce5dc5a5b8b0040b4a5e0e5edf426
ValerieAttila09 <akunvalerie76@gmail.com>
2025-08-10 02:07:30 +0700

    committed

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { PrismaClient } from "@prisma/client"
-
-// const express = require('express')
-// const cors = require('cors')
-// const { PrismaClient } = require('@prisma/client')
+import { requireAuth } from "@clerk/clerk-sdk-node"
 
 const app = express()
 const prisma = new PrismaClient()
@@ -15,6 +12,34 @@ app.use(cors({
   allowedHeaders: ["Content-Type"]
 }))
 app.use(express.json())
+
+app.get("/me", requireAuth(), async (req, res) => {
+  const clerkUserId = req.auth.clerkUserId
+
+  let user = await prisma.user.findUnique({
+    where: {
+      clerkId: clerkUserId
+    }
+  })
+
+  if (!user) {
+    const clerkUser = await fetch(`https://api.clerk.dev/v1/users/${clerkUserId}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`
+      }
+    }).then(r => r.json())
+
+    user = await prisma.user.create({
+      data: {
+        clerkId: clerkUserId,
+        email: clerkUser.email_addresses[0].email_address,
+        name: `${clerkUser.first_name || ""} ${clerkUser.last_name || ""}`.trim()
+      }
+    })
+
+    res.json(user)
+  }
+})
 
 app.get("/users", async (_, res) => {
   const users = await prisma.user.findMany()
@@ -34,5 +59,5 @@ app.post("/users", async (req, res) => {
 })
 
 app.listen(5000, () => {
-  console.log("Server berjalan di http://localhost:3000")
+  console.log("Server berjalan di http://localhost:5000")
 })
