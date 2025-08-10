52c05899e4b198b0642e3f4a8a7cdc44a79785ac
ValerieAttila09 <akunvalerie76@gmail.com>
2025-08-10 22:26:36 +0700

    committed

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { clerk } from "../utils/clerkClient"
+import { clerk } from "../utils/clerkClient.js"
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
