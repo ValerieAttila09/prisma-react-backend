44f8b22a058d1b6447d1f1c23c45b61308d13bc2
ValerieAttila09 <akunvalerie76@gmail.com>
2025-08-11 20:35:43 +0700

    committed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import express from "express"
 import cors from "cors"
 import authRoutes from "./routes/auth.js"
 import webhookRoutes from "./routes/webhook.js"
-import { withAuth } from "@clerk/express"
 
 const app = express()
 
-app.use(withAuth())
 app.use(express.json())
 app.use(cors({
   origin: ["http://localhost:5173", "https://frontend-domain.com"],
@@ -17,7 +15,6 @@ app.get("/", (req, res) => {
   res.send("Backend berjalan!")
 })
 app.use("/", webhookRoutes)
-
 app.use(authRoutes)
 
 const PORT = process.env.PORT || 5000
